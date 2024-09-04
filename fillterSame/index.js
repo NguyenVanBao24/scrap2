26bef0c108ffa0811752e44ebdfc8bb67af72701
nhoden2k <nguyenvanbaom1515@gmail.com>
2024-09-04 08:41:51 +0700

    DATAa

--- fillterSame/index.js
+++ fillterSame/index.js
@@ -1,9 +1,9 @@
 const { Fillter } = require("./filter");
 const { locationss, names } = require("../Web/nameOfSearch");
-
+const StorePath = require("./storePath");
 const dataFillter = [];
 for (let i = 0; i < locationss.length; i++) {
-  const data_Advertsing = require(`../Web/ok/${locationss[i]}_results.json`);
+  const data_Advertsing = require(`./ok/${locationss[i]}_results.json`);
   const data = Fillter(data_Advertsing, "name");
-  StorePath(locationss, data);
+  StorePath(locationss[i], data);
 }
